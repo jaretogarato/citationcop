e2f20f4cd29d510474a725482a753f2bd404c8fb
jaretogarato <jaredgg@gmail.com>
2024-11-16 11:07:36 -0700

    no muffin

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,6 @@ export default async function RootLayout({ children }: PropsWithChildren) {
         <Suspense>
           <Toaster />
         </Suspense>
-        <p>Muffin</p>
       </body>
     </html>
   );
