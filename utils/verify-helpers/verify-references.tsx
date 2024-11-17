868bbc8ef5f7337176efe289b0d56976a548aed0
matthewlongshore <matthewlongshore@gmail.com>
2024-11-17 10:09:43 +0000

    small tweaks

--- utils/verify-helpers/verify-references.tsx
+++ utils/verify-helpers/verify-references.tsx
@@ -8,16 +8,16 @@ import { verifyGoogleSearchResultWithLLM, verifyURL } from "@/actions/openAI-ver
 export async function verifyReference(reference: Reference): Promise<{ isValid: boolean, message: string, source?: string }> {
     
     // removing crossref verification because it is done in Grobid
-    1. 
+    // here we add in the consolidate from Grobid?
 
     // 2. Verify via URL accessibility
-    if (reference.url) {
+    /*if (reference.url) {
         const urlResult = await verifyURL(reference)
         //console.log("URL Result:", urlResult);
         if (urlResult.isValid) {
             return { ...urlResult, source: "URL" };
         }
-    }
+    }*/
 
     // 3. Fallback: Verify via Google Search
     const googleSearchResult = await verifyGoogleSearch(reference)
@@ -30,7 +30,7 @@ export async function verifyReference(reference: Reference): Promise<{ isValid: 
     } else {
         //console.log("Google Search Result faild somehow!?:", googleSearchResult);
         //return { isValid: false, message: googleSearchResult.message };
-        return { isValid: false, message: "Couldn't find anything on the web on this one." };
+        return { isValid: false, message: "Hmmm, web search failed. Can't confirm or not this reference." };
     }
 }
 
@@ -49,10 +49,10 @@ async function verifyGoogleSearch(reference: Reference): Promise<{ isValid: bool
             includeUrl ? reference.url : null, // Only include URL if includeUrl is true
         ]
             .filter((field) => field !== null && field !== undefined)
-            .join(" ");
+            .join(" ")
 
-        return fields;
-    };
+        return fields
+    }
 
     // Perform search and handle results
     const performSearch = async (query: string) => {
@@ -67,7 +67,7 @@ async function verifyGoogleSearch(reference: Reference): Promise<{ isValid: bool
             }
             return { success: false, results: null };
         } catch (error) {
-            console.error("VerifyGoogleSearch: Error in Google search verification:", error);
+            console.error("VerifyGoogleSearch: Error in Google search verification:", error)
             return { success: false, results: null };
         }
     };
