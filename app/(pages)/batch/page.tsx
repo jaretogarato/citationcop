27fd06aaa0868efaf5b37e081eb87e76f055c8d0
matthewlongshore <matthewlongshore@gmail.com>
2025-01-10 09:02:47 +0000

    more organizing

--- app/(pages)/batch/page.tsx
+++ app/(pages)/batch/page.tsx
@@ -3,7 +3,7 @@ import PDFProcessor from '@/app/components/batch/PDFProcessor';
 export default function Page() {
   return (
     <div>
-      <h1>PDF Processing</h1>
+      
       <PDFProcessor />
     </div>
   );
