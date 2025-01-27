bffbe9dd90ef097eabb82a3fb8fcbc8be470df40
matthewlongshore <matthewlongshore@gmail.com>
2025-01-27 07:57:05 +0000

    fixing the card issue .. strange

--- app/(pages)/signin/[id]/page.tsx
+++ app/(pages)/signin/[id]/page.tsx
@@ -1,58 +1,58 @@
-import Logo from '@/app/components/icons/Logo';
+import Logo from '@/app/components/icons/Logo'
 //import { createClient } from '@/utils/supabase/server';
-import { createClient } from '@/app/utils/supabase/client';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+import { createClient } from '@/app/utils/supabase/client'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import {
   getAuthTypes,
   getViewTypes,
   getDefaultSignInView,
   getRedirectMethod
-} from '@/app/utils/auth-helpers/settings';
-import Card from '@/app/components/ui/Card';
-import PasswordSignIn from '@/app/components/ui/AuthForms/PasswordSignIn';
-import EmailSignIn from '@/app/components/ui/AuthForms/EmailSignIn';
-import Separator from '@/app/components/ui/AuthForms/Separator';
-import OauthSignIn from '@/app/components/ui/AuthForms/OauthSignIn';
-import ForgotPassword from '@/app/components/ui/AuthForms/ForgotPassword';
-import UpdatePassword from '@/app/components/ui/AuthForms/UpdatePassword';
-import SignUp from '@/app/components/ui/AuthForms/Signup';
+} from '@/app/utils/auth-helpers/settings'
+import { Card } from '@/app/components/ui/Card'
+import PasswordSignIn from '@/app/components/ui/AuthForms/PasswordSignIn'
+import EmailSignIn from '@/app/components/ui/AuthForms/EmailSignIn'
+import Separator from '@/app/components/ui/AuthForms/Separator'
+import OauthSignIn from '@/app/components/ui/AuthForms/OauthSignIn'
+import ForgotPassword from '@/app/components/ui/AuthForms/ForgotPassword'
+import UpdatePassword from '@/app/components/ui/AuthForms/UpdatePassword'
+import SignUp from '@/app/components/ui/AuthForms/Signup'
 
 export default async function SignIn({
   params,
   searchParams
 }: {
-  params: { id: string };
-  searchParams: { disable_button: boolean };
+  params: { id: string }
+  searchParams: { disable_button: boolean }
 }) {
-  const { allowOauth, allowEmail, allowPassword } = getAuthTypes();
-  const viewTypes = getViewTypes();
-  const redirectMethod = getRedirectMethod();
+  const { allowOauth, allowEmail, allowPassword } = getAuthTypes()
+  const viewTypes = getViewTypes()
+  const redirectMethod = getRedirectMethod()
 
   // Declare 'viewProp' and initialize with the default value
-  let viewProp: string;
+  let viewProp: string
 
   // Assign url id to 'viewProp' if it's a valid string and ViewTypes includes it
   if (typeof params.id === 'string' && viewTypes.includes(params.id)) {
-    viewProp = params.id;
+    viewProp = params.id
   } else {
     const preferredSignInView =
-      cookies().get('preferredSignInView')?.value || null;
-    viewProp = getDefaultSignInView(preferredSignInView);
-    return redirect(`/signin/${viewProp}`);
+      cookies().get('preferredSignInView')?.value || null
+    viewProp = getDefaultSignInView(preferredSignInView)
+    return redirect(`/signin/${viewProp}`)
   }
 
   // Check if the user is already logged in and redirect to the account page if so
-  const supabase = createClient();
+  const supabase = createClient()
 
   const {
     data: { user }
-  } = await supabase.auth.getUser();
+  } = await supabase.auth.getUser()
 
   if (user && viewProp !== 'update_password') {
-    return redirect('/');
+    return redirect('/')
   } else if (!user && viewProp === 'update_password') {
-    return redirect('/signin');
+    return redirect('/signin')
   }
 
   return (
@@ -109,5 +109,5 @@ export default async function SignIn({
         </Card>
       </div>
     </div>
-  );
+  )
 }
